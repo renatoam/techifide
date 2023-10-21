7d99eaf18012c97e65111047020dd2cfda975983
Renato <renato.melo@ab-inbev.com>
2023-10-21 19:09:45 -0300

    feat: create switch theme toggle

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,20 +1,29 @@
 import { Icon, Typography, Wrapper } from '@/components/atoms';
 import { faMoon } from '@fortawesome/free-solid-svg-icons';
-import { memo } from 'react';
+import { memo, useState } from 'react';
 import styles from './Header.module.scss';
  
-function Header() {  
+function Header() {
+  const [mode, setMode] = useState('Dark')
+
+  function switchTheme() {
+    const root = document.querySelector('html')
+    root?.classList.toggle('dark')
+    const newMode = root?.classList.contains('dark') ? 'Light' : 'Dark'
+    setMode(newMode) 
+  }
+  
   return (
     <Wrapper element="header" className={styles.header} aria-label="header">
       <Wrapper className={styles.header__container}>
         <Typography element='h1'>Where in the world?</Typography>
-        <Wrapper className={styles.header__theme} role="button">
-          <Icon
-            label="theme"
-            icon={faMoon}
-          />
-          <Typography element="p">Dark Mode</Typography>
-        </Wrapper>
+          <Wrapper className={styles.header__theme} onClick={switchTheme} role="button">
+            <Icon
+              label="theme"
+              icon={faMoon}
+            />
+            <Typography element="p">{mode} Mode</Typography>
+          </Wrapper>
       </Wrapper>
     </Wrapper>
   )
